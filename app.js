8bfc05c316d9aab4415e242fa2e109cee6230041
pakhtunbilal <122631383+pakhtunbilal@users.noreply.github.com>
2023-03-28 22:38:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets",
+  callbackURL: "https://fair-erin-sea-lion-ring.cyclic.app/auth/google/secrets",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 async function (accessToken, refreshToken, profile, done) {
